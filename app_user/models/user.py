d8174f6175a9dd9cf129b0fc7d455a071c49c619
CHaKU <kuover@gmail.com>
2025-06-26 09:46:35 +0700

    create user

--- app_user/models/user.py
+++ app_user/models/user.py
@@ -4,6 +4,7 @@ from enum import Enum
 
 from base_models.basemodel import BaseClass, BaseEmbedded
 
+
 class UserStatus(Enum):
     Hire = 1
     Quit = 2
@@ -14,34 +15,34 @@ class UserStatus(Enum):
 class RoleUser(BaseEmbedded):
     # -- เก็บ id เวลา load ทำการ lazy loading org จาก db
     orgId = me.ReferenceField('app_organization.models.Organization')
-    orgNameEN = me.StringField()# -- snapshot ของ org name ไว้
+    orgNameEN = me.StringField(null= True, required= False, default = None)# -- snapshot ของ org name ไว้
 
     # -- เก็บ id เวลา load ทำการ lazy loading position จาก db
-    posId = me.ReferenceField('app_position.models.position')
-    posNameEN = me.StringField()# -- snapshot ของ position name ไว้
+    posId = me.ReferenceField('app_position.models.Position')
+    posNameEN = me.StringField(null= True, required= False, default = None)# -- snapshot ของ position name ไว้
 
-    isActive = me.BooleanField()
-    isDelete = me.BooleanField()
-    note = me.StringField()
+    isActive = me.BooleanField(null= True, required= False, default = None)
+    isDelete = me.BooleanField(null= True, required= False, default = None)
+    note = me.StringField(null= True, required= False, default = None)
 
 class User(BaseClass):
     id = me.ObjectIdField(primary_key= True, default= lambda: ObjectId())
     code = me.StringField()
-    fNameTH = me.StringField()
-    lNameTH = me.StringField()
+    fNameTH = me.StringField(null= True, required= False, default = None)
+    lNameTH = me.StringField(null= True, required= False, default = None)
     fNameEN = me.StringField()
     lNameEN = me.StringField()
-    nickName = me.StringField()
-    nation = me.StringField()
-    email = me.StringField()
-    phone = me.StringField()
-    birthDay = me.DateTimeField()
+    nickName = me.StringField(null= True, required= False, default = None)
+    nation = me.StringField(null= True, required= False, default = None)
+    email = me.StringField(null= True, required= False, default = None)
+    phone = me.StringField(null= True, required= False, default = None)
+    birthDay = me.DateTimeField(null= True, required= False, default = None)
     startJobDate = me.DateTimeField()
     status = me.EnumField(UserStatus)
     isAdmin = me.BooleanField()
     isActive = me.BooleanField()
-    note = me.StringField()
-    address = me.StringField()
+    note = me.StringField(null= True, required= False, default = None)
+    address = me.StringField(null= True, required= False, default = None)
     roles = me.EmbeddedDocumentListField(RoleUser)
 
 
--- app_user/views.py
+++ app_user/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from typing import List
 from django.http import HttpRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
@@ -5,7 +7,8 @@ from django.contrib import messages
 
 from app_organization.models import Organization
 from app_position.models import Position
-from app_user.models.user import User, UserStatus
+from app_user.models.user import RoleUser, User, UserStatus
+from base_models.basemodel import UserSnapshot
 
 # Create your views here.
 def index(request: HttpRequest):
@@ -15,6 +18,87 @@ def addUser(request: HttpRequest):
     if request.method == "POST":
         response = HttpResponseRedirect(reverse('indexUser'))
         try:
+            code = request.POST.get("code")
+            if not code:
+                messages.error(request, "Code is required")
+                return response
+            fNameTH = request.POST.get("fnameth")
+            lNameTH = request.POST.get("lnameth")
+            fNameEN = request.POST.get("fnameeng")
+            if not fNameEN:
+                messages.error(request, "First Name EN is required")
+                return response
+            lNameEN = request.POST.get("lnameeng")
+            if not lNameEN:
+                messages.error(request, "Last Name EN is required")
+                return response
+            nickName = request.POST.get("nickname")
+            nation = request.POST.get("nation")
+            email = request.POST.get("email")
+            phone = request.POST.get("phone")
+            address = request.POST.get("address")
+            note = request.POST.get("note")
+            birthday = request.POST.get("birthday")
+            if birthday:
+                birthday = datetime.strptime(birthday, "%d/%m/%Y")
+            sDate = request.POST.get("sdate")
+            if sDate:
+                sDate = datetime.strptime(sDate, "%d/%m/%Y")
+            status = request.POST.get("status")
+            isadmin = True if request.POST.get("isadmin") == 'on' else False
+
+            print(code, fNameTH, lNameTH, fNameEN, lNameEN, nickName, nation, email, phone, address, note, birthday, sDate, status, isadmin)
+
+            posList = request.POST.getlist("pos")
+            orgList = request.POST.getlist("org")
+
+            roleData =[{"posId": pos, "orgId": org} for pos, org in zip(posList, orgList)]
+            roleList: List[RoleUser] = list()
+            for role in roleData:
+                posId = role.get("posId")
+                orgId = role.get("orgId")
+                if posId and orgId:
+                    pos: Position = Position.objects.get(id = posId)
+                    org: Organization = Organization.objects.get(id = orgId)
+                    if pos and org:
+                        roleUser = RoleUser()
+                        roleUser.posId = pos
+                        roleUser.posNameEN = pos.nameEN
+                        roleUser.orgId = org
+                        roleUser.orgNameEN = org.nameEN
+                        roleUser.isActive = True
+                        roleUser.isDelete = False
+                        roleList.append(roleUser)
+
+            print(roleData)
+
+            user = User()
+            user.code = code
+            user.fNameTH = fNameTH
+            user.lNameTH = lNameTH
+            user.fNameEN = fNameEN
+            user.lNameEN = lNameEN
+            user.nickName = nickName
+            user.nation = nation
+            user.email = email
+            user.phone = phone
+            user.address = address
+            user.note = note
+            user.birthDay = birthday
+            user.startJobDate = sDate
+            user.status = UserStatus(int(status))
+            user.isAdmin = isadmin
+            user.isActive = True
+            user.roles = roleList
+            currentUser: User = request.currentUser
+            if currentUser:
+                uCreate = UserSnapshot().UserToSnapshot(currentUser)
+                if uCreate:
+                    user.createBy = uCreate
+            
+            user.save()
+
+            messages.success(request, 'Save Success')
             return response
         except Exception as e:
             print(e)
