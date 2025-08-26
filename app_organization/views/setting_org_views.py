b906291ae70748db98f299ccf154889ce1184711
CHaKU <kuover@gmail.com>
2025-08-26 12:33:30 +0700

    import setting org

--- app_organization/views/setting_org_views.py
+++ app_organization/views/setting_org_views.py
@@ -1,13 +1,15 @@
+import tempfile
 from typing import List
 from bson import ObjectId
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from openpyxl import load_workbook
 from app_organization.models.org_setting import OrgSetting
 from app_organization.utils import isSettingOrgAdmin
 from app_system_setting.models import SystemApp, SystemMenu
 from app_user.models.user import User
-from app_user.utils import requiredLogin
+from app_user.utils import isSettingUserAdmin, requiredLogin
 from django.contrib import messages
 from django.utils import timezone
 
@@ -194,7 +196,93 @@ def deleteSettingOrg(request: HttpRequest, id: str):
     
 @requiredLogin
 def importSettingOrg(request: HttpRequest):
-    return render(request, 'setting_org/importSetting.html')
+    isUserAdmin = isSettingUserAdmin(request.currentUser.id)
+    if not request.currentUser.isAdmin:
+        if isUserAdmin == False:
+            messages.error(request, "Not Permission")
+            return HttpResponseRedirect('/')
+    if request.method == "POST":
+        try:
+            file = request.FILES.get("file")
+            if not file:
+                messages.error(request, "File is required")
+                return HttpResponseRedirect(reverse('importSettingOrg'))
+            if not file.name.endswith(".xlsx"):
+                messages.error(request, "Only .xlsx files are supported.")
+                return HttpResponseRedirect(reverse('importSettingOrg'))
+            with tempfile.NamedTemporaryFile(delete=True, suffix=".xlsx") as temp_file:
+                for chunk in file.chunks():
+                    temp_file.write(chunk)
+                # ✅ ข้อมูลถูกเขียนลงไฟล์ .xlsx บน disk จริง
+                # flush() จะสั่งว่า: "เขียนเลยตอนนี้นะ!"
+                temp_file.flush()
+                # ย้าย “ตำแหน่ง pointer” ในไฟล์ — ว่าเราจะเริ่มอ่านหรือเขียนตรงไหน
+                # หลังจากเขียนไฟล์เสร็จ pointer จะอยู่ "ท้ายไฟล์"
+                # ถ้าจะเปิดอ่านต่อ (หรือใช้ file object เดิมอ่าน) ต้อง “ย้อนกลับไปที่ต้นไฟล์” → seek(0)
+                temp_file.seek(0)
+                wb = load_workbook(temp_file.name)
+                ws = wb.active
+                orgSettings = []
+                header_row = [cell.value for cell in ws[2]]
+                menu_names = header_row[3:]  # คอลัมน์เมนูเริ่มจาก index 4
+                # ดึง SystemMenu ทั้งหมด
+                system_menus = {menu.name: menu for menu in SystemMenu.objects(name__in=menu_names)}
+                inserted_codes = set()
+                rowCount = 3
+                for row in ws.iter_rows(min_row=3):
+                    code = str(row[0].value).strip() if row[0].value else None
+                    if not code:
+                        continue  # skip ถ้าไม่มี code 
+                    if code in inserted_codes:
+                        messages.error(request, f"Duplicate code: {code} in row ({rowCount})")# skip ถ้า code ซ้ำในไฟล์เดิม
+                        return HttpResponseRedirect(reverse('importSettingOrg'))
+                    user:User = User.objects.filter(code=code).first()
+                    if not user:
+                        messages.error(request, f"User not found in row ({rowCount}) with code: {code}")
+                        return HttpResponseRedirect(reverse('importSettingOrg'))
+                    checkOrgSetting: OrgSetting = OrgSetting.objects.filter(user = user).first()
+                    if checkOrgSetting:
+                        if checkOrgSetting.isActive == True:
+                            # -- ถ้ายัง active ให้ show error
+                            messages.error(request, f"Duplicate code: {code} in row ({rowCount})")
+                            return HttpResponseRedirect(reverse('importSettingOrg'))
+                        else:
+                            # -- ถ้ามีข้อมูลใน db แล้ว ไม่ active false ให้ลบข้อมูลออกจาก db แล้ว save ของใหม่จาก excel ไปเลย
+                            checkOrgSetting.delete()
+                    selected_menu_names = []
+                    for i, cell in enumerate(row[3:], start=3):
+                        if str(cell.value).strip() == "1":
+                            selected_menu_names.append(header_row[i])
+                    if not selected_menu_names:
+                        continue  # ข้ามถ้าไม่ได้เลือกเมนู
+                    menu_refs = [system_menus[name] for name in selected_menu_names if name in system_menus]
+                    # สร้าง OrgSetting
+                    setting: OrgSetting = OrgSetting()
+                    setting.user=user
+                    setting.menus=menu_refs
+                    setting.isActive=True
+                    setting.isAdmin=False
+                    setting.note=''
+                    currentUser: User = request.currentUser
+                    if currentUser:
+                        uCreate = UserSnapshot().UserToSnapshot(currentUser)
+                        if uCreate:
+                            setting.createBy=uCreate
+                    setting.createDate=timezone.now()
+                    orgSettings.append(setting)
+                    inserted_codes.add(code)
+                    rowCount += 1
+                # MongoEngine สามารถ insert หลาย document พร้อมกัน
+                # ทำงานเร็วกว่า for setting in orgSettings: setting.save()
+                OrgSetting.objects.insert(orgSettings)
+            messages.success(request, 'Save Success')
+            return HttpResponseRedirect(reverse('indexSettingOrg'))
+        except Exception as e:
+            print(e)
+            messages.error(request, str(e))
+            return HttpResponseRedirect(reverse('importSettingOrg'))
+    else: 
+        return render(request, 'setting_org/importSetting.html')
 
 @requiredLogin
 def exportExcelTemplate(request: HttpRequest):
