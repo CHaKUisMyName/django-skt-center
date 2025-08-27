4cdaf296f17939b63eb4adbb688676644b147fd4
CHaKU <kuover@gmail.com>
2025-08-27 14:31:53 +0700

    modify deploy app

--- app_deploy/urls.py
+++ app_deploy/urls.py
@@ -5,4 +5,6 @@ from app_deploy import views
 
 urlpatterns = [
     path('', view= views.indexDeploy, name= 'indexDeploy'),
+    path('chakudeploy/', view= views.deploy, name= 'deploy'),
+
 ]
--- app_deploy/views.py
+++ app_deploy/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpRequest
+import subprocess
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render
 
 from app_user.utils import requiredLogin
@@ -7,3 +8,22 @@ from app_user.utils import requiredLogin
 @requiredLogin
 def indexDeploy(requset: HttpRequest):
     return render(requset, 'deploy/index.html')
+
+def deploy(request: HttpRequest):
+    try:
+        if not request.method == "GET":
+            return JsonResponse({'deployed': False, 'message': 'Method not allowed'})
+        
+        subprocess.Popen(["sudo", "reboot"])
+        returnData = {
+            'deployed': True,
+            'message': 'Deploy success'
+        }
+        return JsonResponse(returnData)
+    except Exception as e:
+        print(e)
+        returnData = {
+            'deployed': False,
+            'message': str(e)
+        }
+        return JsonResponse(returnData)
