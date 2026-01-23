f56f1a08db0e86995e8628639d1688a9c2185372
CHaKU <kuover@gmail.com>
2026-01-23 16:25:49 +0700

    kuy

--- scheduler.py
+++ scheduler.py
@@ -8,13 +8,13 @@ from app_welcome_board.tasks import update_welcome_statuses
 
 def start():
     scheduler = BackgroundScheduler()
-    # # รันทุก 5 วินาที
-    # scheduler.add_job(update_welcome_statuses, 'interval', seconds=5)
-    # # รันทุกเที่ยงคืน
-    # scheduler.add_job(check_immigration_expired, 'cron', hour=0, minute=0)
-    # # ✅ รันทุกวันเวลา 02:00 (ตี 2)
-    # scheduler.add_job(jobDeleteSessionExpired, 'cron', hour=2, minute=0)
-    # # รันทุกเที่ยงคืน
-    # scheduler.add_job(check_reminder, 'cron', hour=0, minute=0)
+    # รันทุก 5 วินาที
+    scheduler.add_job(update_welcome_statuses, 'interval', seconds=5)
+    # รันทุกเที่ยงคืน
+    scheduler.add_job(check_immigration_expired, 'cron', hour=0, minute=0)
+    # ✅ รันทุกวันเวลา 02:00 (ตี 2)
+    scheduler.add_job(jobDeleteSessionExpired, 'cron', hour=2, minute=0)
+    # รันทุกเที่ยงคืน
+    scheduler.add_job(check_reminder, 'cron', hour=0, minute=0)
 
     scheduler.start()
